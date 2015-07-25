09d5d2370e71dab33be1f8b2fd670116593d1e28
beomseok-zzang <redpepper180@hanmail.net>
2015-07-25 18:54:30 +0900

    textview 연습

--- app/src/main/java/bonkers/cau/sims/MainActivity.java
+++ app/src/main/java/bonkers/cau/sims/MainActivity.java
@@ -2,13 +2,19 @@ package bonkers.cau.sims;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+    TextView textView ;
 
+    //실행되자 마자 실행되는
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        textView= (TextView)findViewById(R.id.bumseok);
+
         setContentView(R.layout.activity_main);
     }
 
