9bd867f0f13fe0c4d79e364be9983c10d082c695
Seunguk <gsces@naver.com>
2015-08-06 12:56:05 +0900

    loading화면 자동 넘김

--- app/src/main/java/bonkers/cau/sims/MainActivity.java
+++ app/src/main/java/bonkers/cau/sims/MainActivity.java
@@ -1,13 +1,13 @@
 package bonkers.cau.sims;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,23 +18,19 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-
-        LayoutInflater inflater = (LayoutInflater) getApplicationContext().
-                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        View myView;
-        myView = inflater.inflate(R.layout.activity_main, null);
-        myView.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        TimerTask myTask = new TimerTask() {
+            public void run() {
                 Intent intent = new Intent(MainActivity.this, ListActivity.class);
                 startActivity(intent);
                 finish();
             }
-        });
-        setContentView(myView);
+        };
+        Timer timer = new Timer();
+        timer.schedule(myTask, 1000);
 
-        textView=(TextView)findViewById(R.id.model);
+        textView = (TextView) findViewById(R.id.model);
         textView.setText(model);
     }
 }
