2528d8125ddd87b0f86fa7f6e64b048183cddcad
beomseok-zzang <redpepper180@hanmail.net>
2015-08-18 13:48:33 +0900

    임시 커밋

--- app/src/main/java/bonkers/cau/sims/KeyBroadCast.java
+++ app/src/main/java/bonkers/cau/sims/KeyBroadCast.java
@@ -3,34 +3,46 @@ package bonkers.cau.sims;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by redpe_000 on 2015-08-11.
  */
 public class KeyBroadCast extends BroadcastReceiver {
+    static final String SYSTEM_DIALOG_REASON_KEY = "reason";
+    private ListDBManager dbManager;
+    private ArrayList<ListData> listDataArrList;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        dbManager= new ListDBManager(context);
+        listDataArrList =dbManager.selectAll();
+        PackageManager packagemanager = context.getPackageManager();
+        List<ApplicationInfo> appList = packagemanager.getInstalledApplications(0);
+        boolean plus,minus=false;
 
 
-        int volume = (Integer)intent.getExtras().get("android.media.EXTRA_VOLUME_STREAM_TYPE");
+        int oldVolume=0;
+        int volume = (Integer)intent.getExtras().get("android.media.EXTRA_VOLUME_STREAM_VALUE");
+        if(volume>oldVolume) {
+            oldVolume=volume;
+            for (ListData data:listDataArrList) {
+                data.getmData1();
+                data.getmData2();
 
+            }
+            PackageManager manager = context.getPackageManager();
+            Intent i = manager.getLaunchIntentForPackage("com.android.settings");
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+            context.startActivity(i);
 
-        Log.d("broadcastt", Integer.toString(volume));
-
-
-
+        }
     }
 }
 
 
 
-
-
-/* @Override
-    public void onReceive(Context context, Intent intent) {
-        PackageManager manager = context.getPackageManager();
-        Intent i = manager.getLaunchIntentForPackage("com.android.settings");
-        i.addCategory(Intent.CATEGORY_LAUNCHER);
-        context.startActivity(i);
-   */
