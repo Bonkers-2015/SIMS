41853e373c2e0ed4bee6388427568af5b6009f3a
bnitech <bntech88@gmail.com>
2015-09-03 23:21:18 +0900

    AddEdit 임시 디자인 수정완료

--- app/src/main/java/bonkers/cau/sims/Buttons.java
+++ app/src/main/java/bonkers/cau/sims/Buttons.java
@@ -1,28 +1,26 @@
 package bonkers.cau.sims;
 
-import android.content.Context;
 import android.graphics.Color;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 
 /**
  * Created by dongbin on 2015-07-31.
  */
 
-public class Buttons extends Button{
+public class Buttons{
 
-    public String name;
+    public String name, title;
     public boolean onOff;
-    public RelativeLayout.LayoutParams params;
+//    public RelativeLayout.LayoutParams params;
     public int onImage,offImage, color;
+    public Button button;
 
-    public Buttons(Context context) {
-        super(context);
+    public Buttons(String title, String name, Button btn) {
 
-        this.name = new String("");
+        this.button = btn;
+//        this.title = title;
+        this.name = name;
         this.onOff = false;
-        this.params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         onImage = R.mipmap.ic_launcher;
         offImage = R.mipmap.ic_launcher;
         color = Color.LTGRAY;
