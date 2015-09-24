6ab41422d23c2b7f057db78813012f35f0c666fd
김광민 <cocomo1316@naver.com>
2015-09-24 18:48:29 +0900

    Sound effect, GPS

--- app/src/main/java/bonkers/cau/sims/Subfunction.java
+++ app/src/main/java/bonkers/cau/sims/Subfunction.java
@@ -1,21 +1,28 @@
 package bonkers.cau.sims;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothManager;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.AudioManager;
+import android.net.Uri;
 import android.net.wifi.WifiManager;
-import android.os.Bundle;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-import android.widget.Toast;
-import android.app.Activity;
+import android.os.Environment;
+import android.provider.Settings;
 import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-/**
- * Created by KGM on 2015-09-19.
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Date;
+
+/*  Created by KGM on 2015-09-19.  */
 public class Subfunction {
+
     /*  Wifi On/Off  */
-    public void toggleWiFi(Context mContext) {
+    public void set_WiFif(Context mContext) {
         WifiManager wifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
         if (!wifiManager.isWifiEnabled()) {
             wifiManager.setWifiEnabled(true);
@@ -25,7 +32,7 @@ public class Subfunction {
     }
 
     /*  Bluetooth On/Off  */
-    public void setBluetooth() {
+    public void set_Bluetooth() {
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (!bluetoothAdapter.isEnabled()) {
             bluetoothAdapter.disable();
@@ -33,4 +40,47 @@ public class Subfunction {
             bluetoothAdapter.enable();
         }
     }
+
+    /*  Silent mode 무음모드로 변경  */
+    public void set_SilentMod(Context mContext) {
+        AudioManager mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        mAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+    }
+
+    /*  Silent mode 진동모드로 변경  */
+    public void set_VibrateMod(Context mContext) {
+        AudioManager mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        mAudioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+    }
+
+    /*  Silent mode 소리모드로 변경  */
+    public void set_RingMod(Context mContext) {
+        AudioManager mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        mAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+    }
+
+    /*  GPS on/off  */
+    public void set_GPS(Context mContext) {
+        Intent intent = new Intent("android.location.GPS_ENABLED_CHANGE");
+        intent.putExtra("enabled", true);
+        mContext.sendBroadcast(intent);
+
+        String provider = Settings.Secure.getString(mContext.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+        if (!provider.contains("gps")) { //if gps is disabled
+            final Intent poke = new Intent();
+            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            mContext.sendBroadcast(poke);
+        } else {
+            final Intent poke = new Intent();
+            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            mContext.sendBroadcast(poke);
+        }
+    }
+
+    /*  Screenshot  */
+
 }
